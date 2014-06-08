9af5fe5a5c257cdb2cef9b18dbf47108d24370e4
Jack <jackxl@hotmail.com>
2014-06-08 21:57:53 +0200

    changed PositionsPackage

--- HNetworkLib/src/nl/avans/hball/networklib/PositionsPackage.java
+++ HNetworkLib/src/nl/avans/hball/networklib/PositionsPackage.java
@@ -1,13 +1,36 @@
 package nl.avans.hball.networklib;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PositionsPackage extends HPackage implements Serializable 
 {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 56996245621303866L;
+	
+	private PlayerPosition _ballPosition;
+	private List<PlayerPosition> _playerPositionList = new ArrayList<PlayerPosition>();
+	
+	public PositionsPackage(PlayerPosition ballPosition, List<PlayerPosition> playerPositionList)
+	{
+		this._ballPosition = ballPosition;
+		this._playerPositionList = playerPositionList;
+	}
+
+	public PlayerPosition get_ballPosition() {
+		return _ballPosition;
+	}
+
+	public void set_ballPosition(PlayerPosition _ballPosition) {
+		this._ballPosition = _ballPosition;
+	}
+
+	public List<PlayerPosition> get_playerPositionList() {
+		return _playerPositionList;
+	}
+
+	public void set_playerPositionList(List<PlayerPosition> _playerPositionList) {
+		this._playerPositionList = _playerPositionList;
+	}
 
 }
